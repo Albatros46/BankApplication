34df08b3fb4669a35fda3f500c4b9b77e5245065
SERVET AKÇADAĞ <34627199+Albatros46@users.noreply.github.com>
2024-09-14 21:35:01 +0200

    Java Spring Boot
    Java Spring Boot  Bank Application

---
+++ src/main/java/com/BankApp/repository/AccountRepository.java
@@ -0,0 +1,10 @@
+package com.BankApp.repository;
+
+import com.BankApp.model.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface AccountRepository extends JpaRepository<Account,Long> {
+    Optional<Account> findByUsername(String username);
+}
